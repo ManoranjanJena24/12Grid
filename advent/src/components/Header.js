805e8909f491bf2c279c07218c12d9ab60c31214
ManoranjanJena24 <manoranjanjenamitsy17@gmail.com>
2024-06-12 09:31:22 +0530

    Added Header

---
+++ advent/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-text">
+      <img src="advent.svg" alt="Logo"  />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
